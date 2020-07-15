335da4a60d23bf20c64cc25a79946f03ef6e5ebe
Colin Nelson <colinnelson1996@gmail.com>
2020-07-15 13:49:48 -0600

    user model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,24 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
