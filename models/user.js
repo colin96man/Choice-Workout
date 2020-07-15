338b3b6bfba2105bf3e182a61c1e3983e712fc3f
Colin Nelson <colinnelson1996@gmail.com>
2020-07-15 16:13:28 -0600

    backend auth

--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
+const SALT_ROUNDS = 6;
+
 const userSchema = new Schema({
     name: {
         type: String,
@@ -20,5 +22,34 @@ const userSchema = new Schema({
 }, {
     timestamps: true
 });
+
+userSchema.set('toJSON', {
+    transform: function(doc, ret) {
+      // remove the password property when serializing doc to JSON
+      delete ret.password;
+      return ret;
+    }
+});
+  
+userSchema.pre('save', function(next) {
+    // 'this' will be set to the current document
+    const user = this;
+    if (!user.isModified('password')) return next();
+    // password has been changed - salt and hash it
+    bcrypt.hash(user.password, SALT_ROUNDS, function(err, hashedPassword) {
+        if (err) return next(err);
+        // replace the user provided password with the hashedPassword
+        user.password = hashedPassword;
+        next();
+    });
+});
+
+userSchema.methods.comparePassword = function(attemptedPassword, cb) {
+    // 'this' represents the document that you called comparePassword on
+    bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
 
 module.exports = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
 const logger = require('morgan');
+const usersRoutes = require('./routes/users');
 
 const app = express();
 
@@ -14,7 +15,10 @@ app.use(express.json());
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'build')));
 
+app.use(require('./config/auth'));
+
 // Put API routes here, before the "catch all" route
+app.use('/api/users', usersRoutes);
 
 // The following "catch all" route (note the *)is necessary
 // for a SPA's client-side routing to properly work
